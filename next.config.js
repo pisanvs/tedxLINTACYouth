6cc44f292d3c280af54a17b772e402d6dde99401
pisanvs <maxmorel@pisanvs.cl>
2025-04-14 13:36:42 -0400

    Fix typo in next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
                 source: '/sponsor_email',
                 destination: 'https://radial-woodpecker-bb5.notion.site/Dossier-TEDxLINTAC-Youth-1d5a179dcaaa80028978c29a680104f3?pvs=73',
                 permanent: true,
+            }
         ]
     },
     images: {
