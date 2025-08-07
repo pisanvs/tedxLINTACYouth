8182865aa61f1cbcffea7b395ffadc4be0acf8c6
pisanvs <maxmorel@pisanvs.cl>
2025-08-07 12:13:35 -0400

    Edit sponsor details

--- src/data/SponsorData.js
+++ src/data/SponsorData.js
@@ -1,6 +1,6 @@
-import sponsor1 from "../../public/sponsors/sponsor1.png";
-import sponsor2 from "../../public/sponsors/sponsor2.png";
-import sponsor3 from "../../public/sponsors/sponsor3.png";
+import ryg from "../../public/sponsors/ryg.png";
+import publicis from "../../public/sponsors/publicis.png";
+import indumac from "../../public/sponsors/indumac.png";
 
 class Sponsor {
   constructor(title, alt, website, image, description = "") {
@@ -14,24 +14,24 @@ class Sponsor {
 
 export const SponsorData = [
   new Sponsor(
-    "Sponsor 1",
-    "Sponsor 1 Logo",
-    "https://example.com/sponsor1",
-    sponsor1,
-    "Our first amazing sponsor supporting the event."
+    "R&G Consultoría",
+    "Logo R&G",
+    "https://rygconsultoria.cl/",
+    ryg,
+    "Consultora Líder en auditoría, contabilidad, y servicios laborales"
   ),
   new Sponsor(
-    "Sponsor 2", 
-    "Sponsor 2 Logo",
-    "https://example.com/sponsor2",
-    sponsor2,
-    "Our second fantastic sponsor helping make this event possible."
+    "Publicis Groupe Chile", 
+    "Logo Publicis Groupe",
+    "https://www.publicisgroupe.com/",
+    publicis,
+    "Líder global en marketing, comunicación y transformación digital, conectando a las más grandes marcas con personas a través de la creatividad y la tecnología."
   ),
   new Sponsor(
-    "Sponsor 3",
-    "Sponsor 3 Logo", 
-    "https://example.com/sponsor3",
-    sponsor3,
-    "Our third incredible sponsor contributing to the success of this event."
+    "Indumac",
+    "Logo Indumac", 
+    "https://www.indumacequipamientos.cl/",
+    indumac,
+    "Empresa chilena líder en mobiliario funcional, con más de 80 años de experiencia, combinando diseño, calidad e innovación para las mejores instituciones de Chile y el extranjero."
   ),
 ];
